d826e833d13fa708dc4f4987ffcd5f74dd912b29
louis baptiste <louisvis@ymail.com>
2018-07-12 03:05:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,7 @@ end
 
 def execute_transaction
   if self.valid? == true && self.status == "pending"
+    binding.pry
     @sender.balance -= @amount
     @receiver.balance += @amount
     self.status = "complete"
