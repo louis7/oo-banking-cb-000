013837d3c01bbdb7616c96165f9d4d9aaea864b8
louis baptiste <louisvis@ymail.com>
2018-07-12 02:40:12 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,14 +1,14 @@
 class Transfer
 attr_reader :sender
 attr_reader :receiver
-attr_reader :money
+attr_reader :amount
 attr_accessor :status
 
 
   def initialize(sender, receiver,money)
     @sender = sender
     @receiver =receiver
-    @money = money
+    @amount = money
     @status = "pending"
   end
 
